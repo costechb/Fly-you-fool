ed13ef9f5209677ea411008cd3174f943cfce9a7
benoit.costechareyre <benoit.costechareyre@iut-valence.fr>
2016-04-29 16:16:00 +0200

    It work !!!

---
+++ src/fr/iutvalence/info/dut/m2107/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.iutvalence.info.dut.m2107;
+
+/**
+ * @author costechb
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
