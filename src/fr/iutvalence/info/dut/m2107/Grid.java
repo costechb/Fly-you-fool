7144ad0098006bf0366d8f22684252bc2ff93107
mahirm <mahirm@PC-B123-06.iut-valence.fr>
2016-05-26 08:07:55 +0200

    Commit de stabilisation de Mehdi

--- src/fr/iutvalence/info/dut/m2107/Grid.java
+++ src/fr/iutvalence/info/dut/m2107/Grid.java
@@ -6,15 +6,27 @@ public class Grid {
 	Tile[] tabgrid;
 	private int nbCol;
 	private int nbLig;
-
+	public Tile[][] map;
+	public static final int MAX_LENGTH_DEFAULT=40;
+	public static final int MAX_WIDTH_DEFAULT=40;
 	/**
 	 * 
 	 * @param x
 	 * @param y
 	 */
-	public Grid(int x, int y) {
+	public Grid() {
 		// TODO - implement Grid.Grid
+		map = new Tile[nbCol][nbLig];
+		for (int i=0; i<MAX_LENGTH_DEFAULT; i++) {
+			for (int j=0; j<MAX_WIDTH_DEFAULT; j++) { 
+				
+				
+			}
+		}
+			
 		throw new UnsupportedOperationException();
+	
+		
 	}
 
 	/**
