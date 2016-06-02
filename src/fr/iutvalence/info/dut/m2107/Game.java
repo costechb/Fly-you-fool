f569b49548186867f42fb494eb935a7207537ae2
Hugo <Hugo@UTILISA-D6VP13O.home>
2016-06-02 10:57:45 +0200

    readKey() implémenté dans IHM_Keyboard et utilisé dans Game

--- src/fr/iutvalence/info/dut/m2107/Game.java
+++ src/fr/iutvalence/info/dut/m2107/Game.java
@@ -15,7 +15,7 @@ public class Game extends JPanel {
 	private Hero hero;
 	private ArrayList<Enemy> enemies;
 	private Grid map;
-	protected IHM_Keyboard keyboard;
+	private IHM_Keyboard keyboard;
 	private IHM_View display;
 	private Tile posSortie;
 	
@@ -78,9 +78,8 @@ public class Game extends JPanel {
 		 while(hero.getPos()!=posSortie && !hero.isCaught(hero)) {
 			 System.out.println("\n Press the direction you want to go by, or Spacebar if you want to stay at your position  \n");
 			 
-			 char direction=keyboard.readKey();
-			 
-			 hero.moveHero();
+			 char direction=keyboard.readkey();
+			 hero.moveHero(direction);
 			 
 			 	enregistrer cette nouvelle position comme sa prochaine position
 			 	faire pareil avec les ennemis
@@ -94,3 +93,79 @@ public class Game extends JPanel {
 		  
 		}
 }
+
+
+/*// TODO - implement IHM_Keyboard.moveHero
+
+// Récupérer la position du héro
+Tile tile = this.hero.getPos();
+// Utiliser le scanner dans le main, récupérer la touche saisi et la passer en paramètre de movehero
+switch (Move) {
+
+case 'z':
+	// Avancer d'une case
+	// Récupérer la case sur laquelle on veut se déplacer
+	Tile tileToMoveForward = this.map.getTile( tile.getX() + 1, tile.getY());
+	// On appelle la méthode de la vérification de la case
+	if (this.hero.checkMov(tileToMoveForward)) {
+		// On lance le déplacement du héro
+		this.hero.moveHero(tileToMoveForward);
+		
+	} else {
+		// La case n'est pas libre
+	}
+	break;
+
+case 'q': 
+	
+	// Aller à gauche d'une case
+	// Récupérer la case sur laquelle on veut se déplacer
+	Tile tileToMoveLeft = this.map.getTile( tile.getX() , tile.getY() + 1 );
+	// On appelle la méthode de la vérification de la case
+	if (this.hero.checkMov(tileToMoveLeft)) {
+		// On lance le déplacement du héro
+		this.hero.moveHero(tileToMoveLeft);
+		
+	} else {
+		// La case n'est pas libre
+	}
+	break;
+	
+case 's':
+	
+	//Reculer d'une case
+	// Récupérer la case sur laquelle on veut se déplacer
+	//TODO: utiliser Grid, les tile n'ont pas de coordonnées
+	Tile tileToMoveBackward = this.map.getTile( tile.getX() - 1 , tile.getY()  );
+	// On appelle la méthode de la vérification de la case
+	if (this.hero.checkMov(tileToMoveBackward)) {
+		// On lance le déplacement du héro
+		this.hero.moveHero(tileToMoveBackward);
+		
+	} else {
+		// La case n'est pas libre
+	}
+	break;
+	
+case 'd': 
+	
+	// Aller à droite d'une case
+	// Récupérer la case sur laquelle on veut se déplacer
+	//TODO: utiliser Grid, les tile n'ont pas de coordonnées
+	Tile tileToMoveRight = this.map.getTile( tile.getX()  , tile.getY() - 1);
+	// On appelle la méthode de la vérification de la case
+	if (this.hero.checkMov(tileToMoveRight )) {
+		// On lance le déplacement du héro
+		this.hero.moveHero(tileToMoveRight );
+		
+	} else {
+		// La case n'est pas libre
+	}
+	
+	
+	break;
+
+default: 
+	// Dans le cas ou la lettre reçu n'est pas valide, on lève une exception
+	throw new IHM_KeyboardException();	
+}	*/
--- src/fr/iutvalence/info/dut/m2107/IHM_Keyboard.java
+++ src/fr/iutvalence/info/dut/m2107/IHM_Keyboard.java
@@ -1,4 +1,5 @@
 package fr.iutvalence.info.dut.m2107;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class IHM_Keyboard {
@@ -103,11 +104,15 @@ public class IHM_Keyboard {
 		}	
 	}
 	
-	public void readkey(){
-		
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		char letter = str.charAt(0);
+	public char readkey(){
+		char letter = 'a';
+		try {
+			letter = (char) System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return letter;
 	}
 	
 	
