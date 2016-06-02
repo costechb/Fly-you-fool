35779eb13b3cf567a97438b1dc9bd41d2d6e4010
Hugo <Hugo@UTILISA-D6VP13O.home>
2016-06-02 17:10:31 +0200

    ajout de la collection ennemies comme paramêtre de isCaught

--- src/fr/iutvalence/info/dut/m2107/Game.java
+++ src/fr/iutvalence/info/dut/m2107/Game.java
@@ -17,7 +17,6 @@ public class Game extends JPanel {
 	private Grid map;
 	private IHM_Keyboard keyboard;
 	private IHM_View display;
-	private Tile posSortie;
 	
     @Override
     /**
@@ -36,16 +35,13 @@ public class Game extends JPanel {
 		this.map = new Grid();
 		System.out.println("\n This is the level you will play on \n");
 		System.out.println("You have to go on the target block at the end on the level \n");
-		this.hero = new Hero();
+		this.hero = new Hero(map.getPosEntree());
 		System.out.println("\n And this is your hero ! \n");
 		this.enemies = new ArrayList<Enemy>();
 		System.out.println("\n But there is some enemies that should not see you. Be careful ! \n");
 	}
     
-	private boolean moveH(Hero theHero, Tile theTile){
-		
-		return true;
-	}
+	
 	
     public void run() throws InterruptedException {
     	//Create a window
@@ -57,28 +53,10 @@ public class Game extends JPanel {
 		myFrame.setSize(750,750);
 		myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		myFrame.setVisible(true);
-		 
-		 /*
-		  * Hugo : Algo du déroulement d'un tour, du jeu etc
-		  * ceci est sujet à changement(s)
-		  * 
-		  * lancer la boucle d'un tour de jeu (tant que la posHero n'est pas la posSortie ou que la posHero n'est pas dans les posChampDeVision)
-		  * 	demander au joueur la direction qu'il veut
-		  * 	enregistrer cette nouvelle position comme sa prochaine position
-		  * 	faire pareil avec les ennemis
-		  * 	actualiser les positions de tout le monde
-		  * 	réafficher la carte
-		  * fin de la boucle d'un tour
-		  * selon la position
-		  * 	sortie : afficher VICTORY
-		  * 	champDeVision : afficher YOU LOOSE
-		  * fin de la methode
-		  */
-
 		/*
 		 * the turn loop
 		 */
-		while(hero.getPos()!=posSortie && !hero.isCaught()) {
+		while(hero.getPos()!=map.getPosSortie() && !hero.isCaught(enemies)) {
 			System.out.println("\n Press the direction you want to go by, or Spacebar if you want to stay at your position  \n");
 			char direction = keyboard.readkey();
 			//System.out.println("\n TESTS : you will now have your mov changed ! \n");
@@ -91,10 +69,10 @@ public class Game extends JPanel {
 			enemies.forEach(enemy->enemy.setPos(enemy.getMov()));
 			display.showMap(map);
 		}
-		if(hero.getPos()==posSortie){
+		if(hero.getPos()==map.getPosSortie()){
 			System.out.println("\n YOU WON \n");
 		}
-		else if (hero.isCaught()){
+		else if (hero.isCaught(enemies)){
 			System.out.println("\n YOU LOOSE \n");
 		}
     }
